51cb9ed3d0a252c93e78f718084fd7940d06e02c
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-04-30 17:22:35 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -10,7 +10,7 @@ import logging
 import aiohttp
 
 
-async def render_page(id, secure_hash):
+async def render_page(id, secure_hash, m):
     file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
