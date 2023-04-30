217993308c0f0dcf550301d18f10a94d5c949f42
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-04-30 23:04:34 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -27,9 +27,7 @@ async def render_page(id, secure_hash):
 
             tag = file_data.mime_type.split('/')[0].strip()
 
-            html = (await r.read()).replace('{online_link}', online_link)
-
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, online_link=online_link)
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, online_link)
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Listen {}'.format(file_data.file_name)
@@ -40,9 +38,7 @@ async def render_page(id, secure_hash):
 
             tag = file_data.mime_type.split('/')[0].strip()
 
-            html = (await r.read()).replace('{online_link}', online_link)
-
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, online_link=online_link)
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, online_link)
     else:
         async with aiofiles.open('Adarsh/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
